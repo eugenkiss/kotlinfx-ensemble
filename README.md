4b01aeb1c754247b920d6cae7ec5605cbfdff9a4
Eugen Kiss <eugen@eugenkiss.com>
2014-05-01 21:58:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+KotlinFX Ensemble
+=================
+
+KotlinFX Ensemble is a recreation of [JavaFX Ensemble][1] in [KotlinFX][2] in
+the same vein as [ScalaFX Ensemble][3].
+
+  [1]: http://www.oracle.com/technetwork/java/javase/overview/javafx-samples-2158687.html
+  [2]: https://github.com/eugenkiss/kotlinfx
+  [3]: https://github.com/scalafx/scalafx-ensemble
+
+For more information see the [KotlinFX Ensemble Wiki](https://github.com/eugenkiss/kotlinfx-ensemble/wiki/).
---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,22 @@
+import kotlinfx.builders.*
+import kotlinfx.properties.*
+import javafx.application.Application
+import javafx.stage.Stage
+
+fun main(args: Array<String>) {
+    Application.launch(javaClass<HelloWorld>())
+}
+
+class HelloWorld : Application() {
+    override fun start(stage: Stage?) {
+        Stage(stage, title = "Hello World!") {
+            scene = Scene(width = 300.0, height = 250.0) {
+                root = StackPane {
+                    + Button("Say 'Hello World'") {
+                        setOnAction { println("Hello World!") }
+                    }
+                }
+            }
+        }.show()
+    }
+}
